37944dd691ed02655ae64aebf1ebd5c96cfab58e
patdavid <patdavid@4517f9da-02dc-11df-9e5a-bdfd8772c6bb>
2010-01-18 22:40:30 +0000

    Minor modifications

--- trunk/lightbox.js
+++ trunk/lightbox.js
@@ -1,1 +1,24 @@
-var lb = {}
+// Creating the lightbox object
+
+//var lightbox = ( function(){ } )();
+//
+/*
+
+var lightbox = ( function(){
+
+    var isOpen = true;
+
+
+    return isOpen;
+
+})();
+
+// There is a whole thing up there to deal with later...
+//========================================
+// Instead, let's go about things this way:
+*/
+
+var lightbox = {};
+lightbox.image = new Image();
+lightbox.bg = document.createElement('div');
+lightbox.bg.id = 'pattr-lightbox-bg';
