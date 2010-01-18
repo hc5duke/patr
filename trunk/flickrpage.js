4bffc8b70c85f04ab072456335cdc890c439c789
patdavid <patdavid@4517f9da-02dc-11df-9e5a-bdfd8772c6bb>
2010-01-18 23:51:13 +0000

    Fleshed out flickrpage.js and lightbox.js.

--- trunk/flickrpage.js
+++ trunk/flickrpage.js
@@ -1,1 +1,20 @@
 // flickrpage.js
+//
+// flickrPage contains functions for dealing with page parameters
+
+var flickrPage = {
+
+   getID : function() {
+             var p = document.location.href.split("/");
+             return p[5];
+           },
+
+   getURL : function() {
+              return document.querySelector("link[rel='canonical']").href ;
+            },
+   
+   getImageURL : function() {
+                   return document.querySelector("link[rel='image_src']").href ;
+                 }
+
+};
