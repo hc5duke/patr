1ab030b52797560184f34a466ee2dcc818141895
patdavid <patdavid@4517f9da-02dc-11df-9e5a-bdfd8772c6bb>
2010-03-25 21:29:27 +0000

    fixed issue with comment testing b0rking buddy reply on a photo page

--- trunk/flickrpage.js
+++ trunk/flickrpage.js
@@ -243,7 +243,7 @@ function doFlickrPage() {
         /* Checking if user has commented on this photo before */
         var comment_urls = document.querySelectorAll("h4[data-ywa-name='Commenter names'] > a:first-of-type");
         for(var key in comment_urls){
-            if( comment_urls[key].href.search( flickrPage.photos_url ) != -1 ){
+            if( comment_urls[key].href && comment_urls[key].href.search( flickrPage.photos_url ) != -1 ){
                 document.querySelector('#DiscussPhoto > h3:first-of-type').insertAdjacentHTML('beforeEnd', " <img src='"+ chrome.extension.getURL('images/icon_comment.gif') +"'/>" );
                 return;
             }
